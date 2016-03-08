ea4de8d6367645dc03dc36b6dff3f8576b5ed642
Alex Mun <axlemn@gmail.com>
2016-03-08 13:43:59 -0800

    Making minimax great

--- player.cpp
+++ player.cpp
@@ -27,8 +27,7 @@ Player::Player(Side side) {
     // HJP: Keep track of the player's current side
     player_side = side;
     // HJP: Keep track of opponent's side
-    if (player_side == WHITE)
-    {
+    if (player_side == WHITE) {
         opponent_side = BLACK;
     }
     else
@@ -43,6 +42,11 @@ Player::Player(Side side) {
  */
 Player::~Player() {
 }
+
+void Player::setBoard(Board *board) {
+    this->board = *board;
+}
+
 
 /*
  * Compute the next move given the opponent's last move. Your AI is
@@ -64,19 +68,16 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
     // HJP: Implementing an AI that greedily picks the move that gets the most
     // points.
 
-    /* AMUN: Pseudocode for using multiple minimax steps
+    /* AMUN: Pseudocode for using multiple minimax steps: to use next week.
     // start_time = timestamp()
-
     // num_moves = number of moves left for US to take
     // end_time = start_time + frac_use[this.moves_made] * msLeft
-    
     end_time = start_time + (msLeft-100) * frac_time
     // Small amount of buffer time allotted to perform the move
     next_move = DEFAULT  TODO: determine default
     */
-
-    std::vector<Board>;
-    board_ahead = this.board.copy();
+    // std::vector<Board>;
+    // board_ahead = this.board.copy();
     /*
     while (current_time < end_time){
         board_ahead.valid_move(); // TODO: What's the return type here?
@@ -111,11 +112,81 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
     }
     else
     {
+        // AMUN: Make a testing partner.  
+        Player test_partner = Player(opponent_side);
         // HJP: Iterate through the list of moves
         for(int i = 0; i < legal_moves.size(); i++)
         {
             // HJP: Clone board into test board for calculating pieces won
             Board * testboard = board.copy();
+
+            // Do our move
+            // testboard->doMove(&legal_moves[i], player_side);
+            // AMUN: Partner tells us what a greedy player would do if 
+            // we make move i
+            test_partner.setBoard(testboard);
+
+            Move * opp_greedy;
+            opp_greedy = test_partner.doSimpleMove(&legal_moves[i]);
+            // Check if test_partner had any moves to make
+            if (opp_greedy != NULL) {
+                testboard->doMove(opp_greedy, opponent_side);
+            }
+
+            if (current_max < testboard->count(player_side))
+            {
+                current_max = testboard->count(player_side);
+                move = &legal_moves[i];
+            }
+            // HJP: Free testboard
+            delete testboard;
+        }
+    }
+    // HJP: dynamically allocate space for returning our greedy move
+    Move * return_move = new Move(move->getX(), move->getY());
+
+    // HJP: Update the board with new move
+    board.doMove(return_move, player_side);
+    return return_move;
+}
+
+
+/*
+ * Does the non-minimax version.  Need this for the minimax version to base 
+ * our recursion off of.
+ */
+Move *Player::doSimpleMove(Move *opponentsMove) {
+    /* 
+     * DONE: Implement how moves your AI should play here. You should first
+     * process the opponent's opponents move before calculating your own move
+     */ 
+    // HJP: Implementing an AI that greedily picks the move that gets the most
+    // points.
+
+
+    // HJP: Update board to process opponent's move
+    board.doMove(opponentsMove, opponent_side);
+    // HJP: We now establish a list of legal moves
+    std::vector<Move> legal_moves = board.possibleMoves(player_side);
+
+    // HJP: Establish a temp. var. holding the max points to gain & the move
+    // that gets those points
+    int current_max = -1;
+    Move * move;
+
+    // HJP: If we have no legal moves
+    if (!legal_moves.size())
+    {
+        return NULL;
+    }
+    else
+    {
+        // HJP: Iterate through the list of moves
+        for(int i = 0; i < legal_moves.size(); i++)
+        {
+            // HJP: Clone board into test board for calculating pieces won
+            Board * testboard = board.copy();
+
             testboard->doMove(&legal_moves[i], player_side);
             if (current_max < testboard->count(player_side))
             {
